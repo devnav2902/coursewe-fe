a714b7ecbda1fb5ada0bb53877cdbcb437925168
Nguyen Anh Vu <63908788+devnav2902@users.noreply.github.com>
2022-02-28 16:43:59 +0700

    chore: add course & user types

---
+++ src/redux/types/course.types.tsx
@@ -0,0 +1,7 @@
+const courseTypes = {
+  COURSE_REQUEST: "COURSE_REQUEST",
+  COURSE_SUCCESS: "COURSE_SUCCESS",
+  COURSE_FAILURE: "COURSE_FAILURE",
+};
+
+export default courseTypes;
---
+++ src/redux/types/user.types.tsx
@@ -0,0 +1,6 @@
+const userTypes = {
+  USER_REQUEST: "USER_REQUEST",
+  USER_SUCCESS: "USER_SUCCESS",
+  USER_FAILURE: "USER_FAILURE",
+};
+export default userTypes;
