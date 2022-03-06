a686e4c3462bb5c068366aa1ee7bd2a1ed543023
Nguyen Anh Vu <63908788+devnav2902@users.noreply.github.com>
2022-03-06 07:26:43 +0700

    chore: add home page file

---
+++ src/pages/home/page/index.page.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUsers } from "../../../redux/actions/user.actions";
+import { Card, Col, Row, Spin } from "antd";
+import axios from "axios";
+import { getAllCourses } from "../../../redux/actions/course.actions";
+const Home = () => {
+  // const dispatch = useDispatch();
+  // const state = useSelector((state) => state);
+
+  // useEffect(() => {
+  //   dispatch(getAllCourses());
+  // }, [dispatch]);
+
+  // const { Meta } = Card;
+
+  return <div style={{ padding: "20px" }}>HOME</div>;
+};
+
+export default Home;
