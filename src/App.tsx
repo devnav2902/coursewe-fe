cd09d74c35a420272ef391c2052b4dfeba82bcd3
Nguyen Anh Vu <63908788+devnav2902@users.noreply.github.com>
2022-03-06 07:57:49 +0700

    chore: update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,60 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import routes from "./routes/allRoutes";
 import { Routes as IRoutes } from "./routes/allRoutes";
+import TopNav from "./components/TopNav/TopNav.component";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { getCurrentUser, login } from "./redux/actions/account.actions";
+import { LoadingOutlined } from "@ant-design/icons";
+import { Spin } from "antd";
+import Loading from "./components/Loading/Loading.component";
+import { ROUTES } from "./utils/constants";
 
 function App(): JSX.Element {
+  const user = useSelector((state: any) => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCurrentUser());
+  }, [dispatch]);
+
+  if (!user.loaded)
+    return (
+      <Loading>
+        <Spin size="large" indicator={<LoadingOutlined />} />
+      </Loading>
+    );
+
   return (
-    <BrowserRouter>
-      <Routes>
-        {routes.map((route: IRoutes, idx: number) => {
-          return (
-            <Route key={idx} path={route.path} element={route.component} />
-          );
-        })}
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <TopNav />
+        <Routes>
+          {routes.map((route: IRoutes, idx: number) => {
+            if (route.redirectIfAuthenticated && user.profile) {
+              return (
+                <Route
+                  key={idx}
+                  path={route.path}
+                  element={<Navigate to="/" />}
+                />
+              );
+            } else if (route.private && !user.profile) {
+              return (
+                <Route
+                  key={idx}
+                  path={route.path}
+                  element={<Navigate to={ROUTES.SIGN_IN} />}
+                />
+              );
+            }
+            return (
+              <Route key={idx} path={route.path} element={route.component} />
+            );
+          })}
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
