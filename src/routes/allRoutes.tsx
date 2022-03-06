d2b0ab82451e56a7e4f7cfe8962178cdb0ee0f71
Nguyen Anh Vu <63908788+devnav2902@users.noreply.github.com>
2022-03-06 07:38:18 +0700

    chore: update routes

--- src/routes/allRoutes.tsx
+++ src/routes/allRoutes.tsx
@@ -1,10 +1,39 @@
-import Home from "../pages/home/pages/index";
+import Home from "../pages/home/page/index.page";
 import NotFound from "../pages/404/pages/index";
+import SigninPage from "../pages/signin/page/signin.page";
+import SignupPage from "../pages/signup/page/signup.page";
+import { ROUTES } from "../utils/constants";
+import PurchaseHistoryPage from "../pages/purchase-history/page/purchaseHistory.page";
 
-export type Routes = { exact?: boolean; path: string; component: JSX.Element };
+export type Routes = {
+  exact?: boolean;
+  path: string;
+  component: JSX.Element;
+  redirectIfAuthenticated?: boolean;
+  private?: boolean;
+};
 
 const routes: Routes[] = [
-  { exact: true, path: "/", component: <Home /> },
+  {
+    exact: true,
+    path: "/",
+    component: <Home />,
+  },
+  {
+    path: ROUTES.SIGN_IN,
+    component: <SigninPage />,
+    redirectIfAuthenticated: true,
+  },
+  {
+    path: ROUTES.SIGN_UP,
+    component: <SignupPage />,
+    redirectIfAuthenticated: true,
+  },
+  {
+    path: ROUTES.PURCHASE_HISTORY,
+    component: <PurchaseHistoryPage />,
+    private: true,
+  },
   { path: "*", component: <NotFound /> },
 ];
 
