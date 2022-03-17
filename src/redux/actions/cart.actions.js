d32ffa23fae405c82a61d13e459f51c43f1995ad
nguyynavu <nguyenanhvu.work@gmail.com>
2022-03-17 14:55:27 +0700

    chore: add shopping cart redux

---
+++ src/redux/actions/cart.actions.js
@@ -0,0 +1,94 @@
+import CourseApi from "../../api/course.api";
+import cartTypes from "../types/cart.types";
+import _ from "lodash";
+
+const addToCart = (id) => async (dispatch) => {
+  dispatch({ type: cartTypes.ADD_TO_CART_REQUEST });
+
+  const { data, status } = await CourseApi.getCourseById(id);
+  console.log(data);
+  if (status === 200) {
+    const {
+      id,
+      title,
+      author: { fullname },
+      thumbnail,
+      slug,
+      rating_avg_rating,
+      // current_discount:{}
+    } = data;
+
+    const course = {
+      id,
+      title,
+      author: { fullname },
+      current_discount: null,
+      thumbnail,
+      slug,
+      rating_avg_rating,
+    };
+
+    dispatch({ type: cartTypes.ADD_TO_CART_SUCCESS, payload: course });
+  }
+};
+
+const removeFromCart = (id) => async (dispatch, getState) => {
+  dispatch({ type: cartTypes.REMOVE_FROM_CART_REQUEST });
+
+  const cartReducer = getState().cart;
+  const cartItems = cartReducer.cart.filter((course) => course.id !== id);
+
+  dispatch({ type: cartTypes.REMOVE_FROM_CART_SUCCESS, payload: cartItems });
+};
+
+const removeFromSavedForLater = (id) => async (dispatch, getState) => {
+  dispatch({ type: cartTypes.REMOVE_FROM_SAVE_FOR_LATER_REQUEST });
+
+  const cartReducer = getState().cart;
+  const cartItems = cartReducer.saved_for_later.filter(
+    (course) => course.id !== id
+  );
+
+  dispatch({
+    type: cartTypes.REMOVE_FROM_SAVE_FOR_LATER_SUCCESS,
+    payload: cartItems,
+  });
+};
+
+const moveToSavedForLater = (id) => async (dispatch, getState) => {
+  dispatch({ type: cartTypes.SAVE_FOR_LATER_REQUEST });
+
+  const cartReducer = getState().cart;
+  const cartItems = cartReducer.cart.filter((course) => course.id !== id);
+
+  const course = cartReducer.cart.find((course) => course.id === id);
+  const savedForLaterItems = _.cloneDeep(cartReducer.saved_for_later);
+  course && savedForLaterItems.push(course);
+
+  const payload = { saved_for_later: savedForLaterItems, cart: cartItems };
+  dispatch({ type: cartTypes.SAVE_FOR_LATER_SUCCESS, payload });
+};
+
+const moveToCart = (id) => async (dispatch, getState) => {
+  dispatch({ type: cartTypes.MOVE_TO_CART_REQUEST });
+
+  const cartReducer = getState().cart;
+  const savedForLaterItems = cartReducer.saved_for_later.filter(
+    (course) => course.id !== id
+  );
+
+  const course = cartReducer.saved_for_later.find((course) => course.id === id);
+  const cartItems = _.cloneDeep(cartReducer.cart);
+  course && cartItems.push(course);
+
+  const payload = { saved_for_later: savedForLaterItems, cart: cartItems };
+  dispatch({ type: cartTypes.MOVE_TO_CART_SUCCESS, payload });
+};
+
+export {
+  addToCart,
+  removeFromCart,
+  removeFromSavedForLater,
+  moveToSavedForLater,
+  moveToCart,
+};
