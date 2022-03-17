d179a14dc75987ade985e556707e4257f0fb8ce6
nguyynavu <nguyenanhvu.work@gmail.com>
2022-03-17 14:58:16 +0700

    chore: add cart reducer to store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -3,6 +3,8 @@ import thunk from "redux-thunk";
 import { createLogger } from "redux-logger";
 import courseReducer from "../reducers/course.reducer";
 import accountReducer from "../reducers/account.reducer";
+import cartReducer from "../reducers/cart.reducer";
+import _ from "lodash";
 
 const logger = createLogger({
   // ...options
@@ -12,6 +14,32 @@ const middleware = [thunk, logger];
 const reducers = combineReducers({
   course: courseReducer,
   user: accountReducer,
+  cart: cartReducer,
 });
-const store = createStore(reducers, applyMiddleware(...middleware));
+
+const persistedState = localStorage.getItem("shoppingCart:storage")
+  ? JSON.parse(localStorage.getItem("shoppingCart:storage"))
+  : {};
+
+const store = createStore(
+  reducers,
+  persistedState,
+  applyMiddleware(...middleware)
+);
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("shoppingCart:storage", serializedState);
+  } catch {
+    // ignore write errors
+  }
+};
+
+store.subscribe(
+  _.throttle(() => {
+    saveState(store.getState());
+  }, 1000)
+);
+
 export default store;
