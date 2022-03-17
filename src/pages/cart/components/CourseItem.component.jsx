2dd4d6f3b8cf08c6bfaa379900cac3417264a60a
nguyynavu <nguyenanhvu.work@gmail.com>
2022-03-17 14:45:58 +0700

    chore: update course component of cart

--- src/pages/cart/components/CourseItem.component.jsx
+++ src/pages/cart/components/CourseItem.component.jsx
@@ -1,28 +1,85 @@
-import React from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import Rating from "../../../components/Rating/Rating.component";
+import {
+  moveToCart,
+  moveToSavedForLater,
+  removeFromCart,
+  removeFromSavedForLater,
+} from "../../../redux/actions/cart.actions";
+import { BE_URL, routesWithParams } from "../../../utils/constants";
+import { isUrl } from "../../../utils/functions";
 
-const CourseItem = (props) => {
-  const { thumbnail, author, title, slug } = props;
+const CourseItem = ({ course, actionType }) => {
+  const { thumbnail, author, title, slug, id, rating_avg_rating } = course;
+  const dispatch = useDispatch();
+
+  const handleRemoveFromCart = (id) => {
+    dispatch(removeFromCart(id));
+  };
+  const handleRemoveFromSavedForLater = (id) => {
+    dispatch(removeFromSavedForLater(id));
+  };
+  const handleMoveToSavedForLater = (id) => {
+    dispatch(moveToSavedForLater(id));
+  };
+  const handleMoveToCart = (id) => {
+    dispatch(moveToCart(id));
+  };
+
+  const Actions = () => {
+    return (
+      <div className="card-action">
+        {actionType === "cart" ? (
+          <>
+            <span
+              className="remove-btn"
+              onClick={() => handleRemoveFromCart(id)}
+            >
+              Xóa
+            </span>
+            <span
+              className="move-to-cart"
+              onClick={() => handleMoveToSavedForLater(id)}
+            >
+              Thanh toán sau
+            </span>
+          </>
+        ) : (
+          <>
+            <span
+              className="remove-btn"
+              onClick={() => handleRemoveFromSavedForLater(id)}
+            >
+              Xóa
+            </span>
+            <span className="move-to-cart" onClick={() => handleMoveToCart(id)}>
+              Chuyển vào giỏ hàng
+            </span>
+          </>
+        )}
+      </div>
+    );
+  };
+
   return (
-    <div className="course-item" data-course="${id}">
-      <a href={"/course/" + slug} className="">
+    <div className="course-item">
+      <Link to={routesWithParams.detail_course(slug)} className="">
         <div className="card-thumbnail">
-          <img src="https://images.unsplash.com/photo-1542995470-870e12e7e14f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
+          <img
+            alt={title}
+            src={isUrl(thumbnail) ? thumbnail : BE_URL + "/" + thumbnail}
+          />
         </div>
         <div className="card-info">
-          <div className="card-info__title">${title}</div>
-          <div className="card-info__instructor">
-            by&nbsp;${author?.fullname}
-          </div>
+          <div className="card-info__title">{title}</div>
+          <div className="card-info__instructor">by&nbsp;{author.fullname}</div>
           <div className="card-info__rating">
-            {/* ${renderRating(rating_avg_rating, rating_count)} */}
+            <Rating value={rating_avg_rating} size="14px" />
           </div>
         </div>
-      </a>
-      <div className="card-action">
-        {/* ${actions} */}
-        <span class="remove-btn">Remove</span>
-        <span class="move-to-cart">Move to cart</span>
-      </div>
+      </Link>
+      <Actions />
       <div className="card-price">
         {/* ${
                   discount === "Free"
