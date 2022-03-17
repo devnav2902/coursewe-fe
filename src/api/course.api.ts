d23638bd264fdf320bdadf3b3551334ea624e5d1
nguyynavu <nguyenanhvu.work@gmail.com>
2022-03-17 15:00:10 +0700

    chore: add api getCourseById & remove api get

--- src/api/course.api.ts
+++ src/api/course.api.ts
@@ -2,22 +2,16 @@ import axios from "axios";
 import axiosClient from "../utils/axios";
 
 class Course {
-  get = async (limit?: number) => {
-    const url = "http://127.0.0.1:8000/api/course";
-    if (limit) {
-      return axios.get(`${url}?limit=${limit}`).then((res) => {
-        return res;
-      });
-    } else {
-      return axios.get(`${url}`).then((res) => {
-        return res;
-      });
-    }
-  };
-
   getCourseBySlug = async (slug: string) => {
     return axiosClient
       .post("/course", { slug })
+      .then((res) => res)
+      .catch((error) => error.response);
+  };
+
+  getCourseById = async (id: string | number) => {
+    return axiosClient
+      .get(`/course/${id}`)
       .then((res) => res)
       .catch((error) => error.response);
   };
