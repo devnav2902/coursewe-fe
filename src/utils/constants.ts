29ce79581a6adad9b7c6b8d64a1a006a39e898cf
nguyynavu <nguyenanhvu.work@gmail.com>
2022-04-12 08:28:57 +0700

    Rename slug & add utils

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,7 +9,7 @@ enum ROUTES {
   "CART" = "/cart",
   "CREATE_COURSE" = "/create-course",
   "INSTRUCTOR_BIO" = "/instructor/profile/:slug",
-  "DETAIL_COURSE" = "/course/:slug",
+  "DETAIL_COURSE" = "/khoa-hoc/:slug",
   "INSTRUCTOR_COURSES" = "/instructor/courses",
   "USER_COURSES" = "/user/courses",
   "OVERVIEW" = "/instructor/overview",
@@ -30,7 +30,7 @@ enum ROUTES {
 }
 
 const routesWithParams = {
-  detail_course: (slug: string) => `/course/${slug}`,
+  detail_course: (slug: string) => `/khoa-hoc/${slug}`,
   instructor_bio: (slug: string) => `/instructor/profile/${slug}`,
   course_draft: (id: number) => `/course/draft/${id}`,
   course_basics: (id: string) => `/instructor/course/${id}/basics`,
