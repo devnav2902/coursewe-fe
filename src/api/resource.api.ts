4abff206a35d483320bd7d733d2a619127dd8635
nguyynavu <nguyenanhvu.work@gmail.com>
2022-04-28 19:26:18 +0700

    Add api: lecture, resource

---
+++ src/api/resource.api.ts
@@ -0,0 +1,57 @@
+import axios, { CancelTokenSource } from "axios";
+import axiosClient from "../utils/axios";
+
+type Upload = {
+  data: FormData;
+  onUploadProgress: (e: ProgressEvent) => void;
+  source: CancelTokenSource;
+};
+class Resource {
+  getByLectureId = async (lectureId: number) => {
+    return axiosClient
+      .get(`/resources/lecture-id/${lectureId}`)
+      .catch((error) => error.response);
+  };
+
+  upload = async ({ data, onUploadProgress, source }: Upload) => {
+    return axiosClient
+      .post("/resources/upload", data, {
+        onUploadProgress,
+        cancelToken: source.token,
+      })
+      .catch((error) => {
+        if (axios.isCancel(error)) {
+          console.log("Request canceled", error);
+        } else {
+        }
+      });
+  };
+
+  delete = async (lectureId: number, courseId: number, resourceId: number) => {
+    return axiosClient
+      .delete(
+        `/user/me/taught-courses/${courseId}/lectures/${lectureId}/resources/${resourceId}/`
+      )
+      .then((res) => res)
+      .catch((error) => error.response);
+  };
+
+  download = async (
+    lectureId: number,
+    courseId: number,
+    resourceId: number
+  ) => {
+    return axiosClient
+      .get(
+        `/users/me/subscribed-courses/${courseId}/lectures/${lectureId}/assets/${resourceId}/download/`,
+        {
+          responseType: "blob",
+        }
+      )
+      .then((res) => res)
+      .catch((error) => error.response);
+  };
+}
+
+const ResourceApi = new Resource();
+export default ResourceApi;
