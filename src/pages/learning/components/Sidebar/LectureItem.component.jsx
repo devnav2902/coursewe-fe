1f85c664eb30321958c572c4a111c119848c0c11
nguyynavu <nguyenanhvu.work@gmail.com>
2022-04-28 19:30:36 +0700

    Feat download resources

--- src/pages/learning/components/Sidebar/LectureItem.component.jsx
+++ src/pages/learning/components/Sidebar/LectureItem.component.jsx
@@ -1,6 +1,12 @@
+import { Dropdown, Popover } from "antd";
 import { useState } from "react";
+import { AiOutlineFileText } from "react-icons/ai";
+import { BsPlayCircle } from "react-icons/bs";
+import { ImFolderDownload } from "react-icons/im";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import ProgressApi from "../../../../api/progress.api";
+import ResourceApi from "../../../../api/resource.api";
 import {
   getProgress,
   getSections,
@@ -40,6 +46,24 @@ const LectureItem = ({ lecture }) => {
     });
   }
 
+  function downloadFile(lectureId, ResourceId, filename) {
+    const {
+      course: { id: courseId },
+    } = dataCourse;
+    ResourceApi.download(lectureId, courseId, ResourceId).then((res) => {
+      console.log(res);
+      const blobFile = res.data;
+      const fileUrl = URL.createObjectURL(blobFile);
+
+      const link = document.createElement("a");
+
+      link.href = fileUrl;
+      link.setAttribute("download", filename); // set custom file name
+
+      link.click(); // force download file without open new tab
+    });
+  }
+
   return (
     <li className="curriculum-item {{ $key == 0 && $lecture->order == 1 ? 'is-current' : '' }} d-flex">
       <div className="progress-toggle">
@@ -59,31 +83,51 @@ const LectureItem = ({ lecture }) => {
           {lecture.order}.&nbsp;{lecture.title}
         </div>
         <div className="bottom d-flex align-items-center">
-          {/* <div className="duration">
-            <i className="fas fa-play-circle"></i>
-            <span className="times">15min</span>
-          </div> */}
+          <div className="duration d-flex align-items-center">
+            <BsPlayCircle style={{ marginRight: 5 }} />
+            <span className="times">{lecture.playtime_string}</span>
+          </div>
 
           {lecture.resource.length > 0 && (
             <div className="resource-list">
-              <button className="dropdown d-flex align-items-center">
-                <i className="fas fa-folder-open"></i>
-                Resources
-                <i className="fas fa-chevron-down"></i>
-              </button>
+              <Dropdown
+                placement="bottomRight"
+                getPopupContainer={(e) => e}
+                overlay={
+                  <ul className="list">
+                    {lecture.resource.map((resource, i) => {
+                      const { lecture_id: lectureId, id: resourceid } =
+                        resource;
 
-              <ul className="list">
-                {lecture.resource.map((resource) => (
-                  <li>
-                    <button className="d-flex download-btn align-items-center">
-                      <i className="fas fa-file-download"></i>
-                      <div className="filename">
-                        {resource.original_filename}
-                      </div>
-                    </button>
-                  </li>
-                ))}
-              </ul>
+                      return (
+                        <li
+                          onClick={() =>
+                            downloadFile(
+                              lectureId,
+                              resourceid,
+                              resource.original_filename
+                            )
+                          }
+                          key={i}
+                          className="cursor-pointer d-flex download-btn align-items-center"
+                        >
+                          <ImFolderDownload style={{ flexShrink: 0 }} />
+                          <div className="ml-1 filename">
+                            {resource.original_filename}
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                }
+                trigger={["click"]}
+              >
+                <button className="dropdown d-flex align-items-center">
+                  <AiOutlineFileText style={{ marginRight: 4 }} />
+                  Tài liệu
+                  <MdOutlineKeyboardArrowDown fontSize={18} className="ml-1" />
+                </button>
+              </Dropdown>
             </div>
           )}
         </div>
