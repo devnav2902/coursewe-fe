66bb4c878b25bac5888bf23bf404f53d370d7f72
nguyynavu <nguyenanhvu.work@gmail.com>
2022-04-28 19:27:52 +0700

    Update component: CurriculumItem

--- src/pages/detail-course/components/CurriculumItem.component.jsx
+++ src/pages/detail-course/components/CurriculumItem.component.jsx
@@ -1,16 +1,17 @@
 import { PlayCircleOutlined } from "@ant-design/icons";
+import { secondsToHMS } from "../../../utils/functions";
 
-const CurriculumItem = ({ title }) => {
+const CurriculumItem = ({ title, playtime_seconds }) => {
   return (
-    <div className="curriculum-item">
+    <div className="curriculum-item d-flex align-items-center">
       {/* <a href="#" class="play-media"> */}
       <div className="pull-left">
         <PlayCircleOutlined style={{ marginRight: "1rem" }} />
         {title}
       </div>
-      {/* <div class="pull-right">
-                  <div class="minutes">35 Minutes</div>
-              </div>  */}
+      <div class="pull-right ml-auto">
+        <div class="duration">{secondsToHMS(playtime_seconds)}</div>
+      </div>
       {/* </a> */}
     </div>
   );
