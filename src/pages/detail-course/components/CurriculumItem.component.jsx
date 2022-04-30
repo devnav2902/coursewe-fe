eeb9be1bca866d1ab2739950f885ee87f1167432
nguyynavu <nguyenanhvu.work@gmail.com>
2022-04-30 21:44:00 +0700

    Remove unuse code

--- src/pages/detail-course/components/CurriculumItem.component.jsx
+++ src/pages/detail-course/components/CurriculumItem.component.jsx
@@ -4,15 +4,13 @@ import { secondsToHMS } from "../../../utils/functions";
 const CurriculumItem = ({ title, playtime_seconds }) => {
   return (
     <div className="curriculum-item d-flex align-items-center">
-      {/* <a href="#" class="play-media"> */}
       <div className="pull-left">
         <PlayCircleOutlined style={{ marginRight: "1rem" }} />
         {title}
       </div>
-      <div class="pull-right ml-auto">
-        <div class="duration">{secondsToHMS(playtime_seconds)}</div>
+      <div className="pull-right ml-auto">
+        <div className="duration">{secondsToHMS(playtime_seconds)}</div>
       </div>
-      {/* </a> */}
     </div>
   );
 };
