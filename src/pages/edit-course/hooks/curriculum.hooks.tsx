ac22bb37526635fea45ace132855fcd6206fc119
devnav2909 <nguyenanhvu.2902@gmail.com>
2022-05-23 16:22:01 +0700

    Add drag and drop for curriculum

--- src/pages/edit-course/hooks/curriculum.hooks.tsx
+++ src/pages/edit-course/hooks/curriculum.hooks.tsx
@@ -1,67 +1,23 @@
-import { useState } from "react";
-import { Lecture, ResourceItems } from "../../../ts/types/course.types";
-import ResourceApi from "../../../api/resource.api";
-import { Modal } from "antd";
-import { ExclamationCircleOutlined } from "@ant-design/icons";
+import {
+  createContext,
+  FC,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import LectureApi from "../../../api/lecture.api";
-
-type DataResources = {
-  resources: ResourceItems;
-  getLatestResources: () => void;
-  deleteResource: (resourceId: number, courseId: number) => void;
-};
-
-export const useResources = (
-  lectureId: number,
-  iniResourcesItems: ResourceItems
-): DataResources => {
-  const [resources, setResources] = useState<ResourceItems>(iniResourcesItems);
-
-  function getLatestResources() {
-    ResourceApi.getByLectureId(lectureId)
-      .then((res) => {
-        console.log(res);
-        setResources(res.data.resources);
-      })
-      .catch((error) => error);
-  }
-
-  function deleteResource(resourceId: number, courseId: number) {
-    Modal.confirm({
-      title: "Thông báo",
-      icon: <ExclamationCircleOutlined />,
-      content: "Bạn xác nhận muốn gỡ tài liệu này khỏi bài học?",
-      okText: "Đồng ý",
-      cancelText: "Hủy bỏ",
-      onOk: () => {
-        if (courseId) {
-          return new Promise((resolve, reject) => {
-            ResourceApi.delete(lectureId, courseId, resourceId)
-              .then((res) => {
-                getLatestResources();
-                resolve(res);
-              })
-              .catch((e) => reject(e));
-          });
-        }
-      },
-      onCancel() {},
-    });
-  }
-
-  return { resources, getLatestResources, deleteResource };
-};
+import { useTypedSelector } from "../../../hooks/redux.hooks";
+import { Lecture } from "../../../ts/types/course.types";
+import { CURRICULUM_TYPES } from "../utils/constants";
 
 type DataLecture = {
   lecture: Lecture;
-  deleted: boolean;
   getLatestLecture: () => void;
-  deleteLecture: (lectureId: number, courseId: number) => void;
 };
 
 export const useLecture = (iniLectureItem: Lecture): DataLecture => {
   const [lecture, setLecture] = useState(iniLectureItem);
-  const [deleted, setDeleted] = useState(false);
 
   function getLatestLecture() {
     LectureApi.getByLectureId(iniLectureItem.id)
@@ -72,26 +28,252 @@ export const useLecture = (iniLectureItem: Lecture): DataLecture => {
       .catch((error) => error);
   }
 
-  function deleteLecture(lectureId: number, courseId: number) {
-    Modal.confirm({
-      title: "Thông báo",
-      icon: <ExclamationCircleOutlined />,
-      content: "Bạn xác nhận muốn xóa bài giảng này khỏi chương học?",
-      okText: "Đồng ý",
-      cancelText: "Hủy bỏ",
-      onOk: () => {
-        return new Promise((resolve, reject) => {
-          LectureApi.delete(lectureId, courseId)
-            .then((res) => {
-              setDeleted(true);
-              resolve(res);
-            })
-            .catch((e) => reject(e));
-        });
-      },
-      onCancel() {},
-    });
-  }
+  return { lecture, getLatestLecture };
+};
 
-  return { lecture, getLatestLecture, deleteLecture, deleted };
+type Func = () => void;
+type HandleData = Func;
+type LectureContext = {
+  nthLecture: number | null;
+  isDisplayEditTitle: boolean;
+  lectureData: {
+    lecture: Lecture | null;
+    updatedTitle: string;
+    handle: {
+      getLatestLecture: HandleData;
+      setUpdatedTitle: (value: string) => void;
+    };
+  };
+  contentTab: {
+    displayContentTab: boolean;
+    handleDisplayContentTab: HandleData;
+    closeContentTab: HandleData;
+  };
+  uploadMedia: {
+    displayUploadMedia: boolean;
+    handleDisplayUploadMedia: HandleData;
+    closeUploadMedia: HandleData;
+  };
+  uploadResources: {
+    displayUploadResources: boolean;
+    handleDisplayUploadResources: HandleData;
+    closeUploadResources: HandleData;
+  };
+  lectureUploading: {
+    setLectureUploadingTo: (value: boolean) => void;
+    lectureUploading: boolean;
+  };
+  resourceUploading: {
+    setResourceUploadingTo: (value: boolean) => void;
+    resourceUploading: boolean;
+  };
+  displayResourceData: {
+    handleDisplayResources: HandleData;
+    displayResources: boolean;
+  };
+};
+
+const initialLectureContext: LectureContext = {
+  nthLecture: null,
+  isDisplayEditTitle: false,
+  lectureData: {
+    lecture: null,
+    updatedTitle: "",
+    handle: {
+      getLatestLecture: () => {},
+      setUpdatedTitle: () => {},
+    },
+  },
+  contentTab: {
+    displayContentTab: false,
+    handleDisplayContentTab: () => {},
+    closeContentTab: () => {},
+  },
+  uploadMedia: {
+    displayUploadMedia: false,
+    handleDisplayUploadMedia: () => {},
+    closeUploadMedia: () => {},
+  },
+  uploadResources: {
+    displayUploadResources: false,
+    handleDisplayUploadResources: () => {},
+    closeUploadResources: () => {},
+  },
+  lectureUploading: {
+    setLectureUploadingTo: () => {},
+    lectureUploading: false,
+  },
+  resourceUploading: {
+    setResourceUploadingTo: () => {},
+    resourceUploading: false,
+  },
+  displayResourceData: {
+    handleDisplayResources: () => {},
+    displayResources: false,
+  },
+};
+export const LectureContext = createContext<LectureContext>(
+  initialLectureContext
+);
+
+type LectureProviderProps = {
+  lecture: Lecture;
+  nthLecture: number;
+};
+export const LectureProvider: FC<LectureProviderProps> = ({
+  children,
+  lecture: lectureItem,
+  nthLecture,
+}) => {
+  const { id, title } = lectureItem;
+
+  const [displayResources, setDisplayResources] = useState<boolean>(false);
+  const [displayUploadResources, setDisplayUploadResources] =
+    useState<boolean>(false);
+  const [displayUploadMedia, setDisplayUploadMedia] = useState<boolean>(false);
+  const [displayContentTab, setDisplayContentTab] = useState<boolean>(false);
+  const [lectureUploading, setLectureUploading] = useState<boolean>(false);
+  const [resourceUploading, setResourceUploading] = useState<boolean>(false);
+  const [updatedTitle, setUpdatedTitle] = useState<string>(title);
+
+  const { elementDisplay } = useTypedSelector((state) => state.curriculum);
+
+  const { getLatestLecture, lecture } = useLecture(lectureItem);
+
+  const isDisplayEditTitle =
+    elementDisplay.id === id &&
+    elementDisplay.type === CURRICULUM_TYPES.LECTURE;
+
+  useEffect(() => {
+    if (isDisplayEditTitle) {
+      setDisplayResources(false);
+      setDisplayUploadResources(false);
+      setDisplayUploadMedia(false);
+      setDisplayContentTab(false);
+    }
+  }, [isDisplayEditTitle]);
+
+  useEffect(() => {
+    if (
+      displayUploadMedia ||
+      displayUploadResources ||
+      lecture.resource.length < 1
+    ) {
+      setDisplayResources(false);
+      setDisplayContentTab(false);
+    }
+  }, [displayUploadMedia, displayUploadResources, lecture.resource]);
+
+  useEffect(() => {
+    if (displayContentTab) {
+      setDisplayResources(false);
+      setDisplayUploadResources(false);
+      setDisplayUploadMedia(false);
+    }
+  }, [displayContentTab]);
+
+  const setLectureUploadingTo = useCallback((value: boolean) => {
+    setLectureUploading(value);
+  }, []);
+
+  const setResourceUploadingTo = useCallback((value: boolean) => {
+    setResourceUploading(value);
+  }, []);
+
+  const handleDisplayUploadResources = useCallback(() => {
+    setDisplayUploadResources(true);
+  }, []);
+
+  // toggle button
+  const handleDisplayResources = useCallback(() => {
+    setDisplayResources((state) => !state);
+  }, []);
+
+  const closeUploadResources = useCallback(() => {
+    setDisplayUploadResources(false);
+  }, []);
+
+  const closeUploadMedia = useCallback(() => {
+    setDisplayUploadMedia(false);
+  }, []);
+
+  const handleDisplayUploadMedia = useCallback(() => {
+    setDisplayUploadMedia(true);
+  }, []);
+
+  const closeContentTab = useCallback(() => {
+    setDisplayContentTab(false);
+  }, []);
+
+  const handleDisplayContentTab = useCallback(() => {
+    setDisplayContentTab(true);
+  }, []);
+
+  const value: LectureContext = useMemo(
+    () => ({
+      isDisplayEditTitle,
+      nthLecture,
+      lectureData: {
+        lecture: updatedTitle ? { ...lecture, title: updatedTitle } : lecture,
+        updatedTitle,
+        handle: {
+          getLatestLecture,
+          setUpdatedTitle,
+        },
+      },
+      contentTab: {
+        displayContentTab,
+        handleDisplayContentTab,
+        closeContentTab,
+      },
+      uploadMedia: {
+        displayUploadMedia,
+        handleDisplayUploadMedia,
+        closeUploadMedia,
+      },
+      uploadResources: {
+        displayUploadResources,
+        handleDisplayUploadResources,
+        closeUploadResources,
+      },
+      lectureUploading: {
+        setLectureUploadingTo,
+        lectureUploading,
+      },
+      resourceUploading: {
+        setResourceUploadingTo,
+        resourceUploading,
+      },
+      displayResourceData: {
+        handleDisplayResources,
+        displayResources,
+      },
+    }),
+    [
+      nthLecture,
+      updatedTitle,
+      isDisplayEditTitle,
+      resourceUploading,
+      lecture,
+      lectureUploading,
+      displayContentTab,
+      displayUploadMedia,
+      displayUploadResources,
+      displayResources,
+      getLatestLecture,
+      handleDisplayResources,
+      handleDisplayUploadMedia,
+      closeUploadResources,
+      closeContentTab,
+      closeUploadMedia,
+      handleDisplayContentTab,
+      handleDisplayUploadResources,
+      setLectureUploadingTo,
+      setResourceUploadingTo,
+    ]
+  );
+
+  return (
+    <LectureContext.Provider value={value}>{children}</LectureContext.Provider>
+  );
 };
