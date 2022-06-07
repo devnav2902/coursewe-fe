a961dcbe26a12e3ddc5d79bafdd1e25b64d9c14c
devnav2909 <nguyenanhvu.2902@gmail.com>
2022-06-07 14:39:31 +0700

    Add types: notification, pagination

---
+++ src/ts/types/notification.types.ts
@@ -0,0 +1,47 @@
+import { Bill } from "./course-bill.types";
+import { Purchased } from "./course.types";
+
+export type NotificationType =
+  | "approved"
+  | "unapproved"
+  | "submit_for_review"
+  | "purchased"
+  | "quality_review";
+
+export type NotificationEntity = {
+  id: number;
+  type: NotificationType;
+  text_start: string;
+  text_end: string;
+};
+
+export type NotificationCourse = {
+  id: number;
+  notification_id: number;
+  course_id: number;
+  course: {
+    author_id: number;
+    id: number;
+    title: string;
+    thumbnail: string;
+    is_purchased: Purchased;
+  };
+};
+
+export type NotificationPurchase = {
+  id: number;
+  notification_id: number;
+  course_bill_id: number;
+  course_bill: Bill;
+};
+
+export type Notification = {
+  id: number;
+  notification_entity_id: number;
+  is_seen: boolean | 1 | 0;
+  created_at: string;
+  updated_at: string;
+  notification_entity: NotificationEntity;
+  notification_course: null | NotificationCourse;
+  notification_purchase: null | NotificationPurchase;
+};
