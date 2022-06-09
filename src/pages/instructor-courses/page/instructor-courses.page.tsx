e954602b7d8dbdf51b3eecee5254ced2cf5e63cc
devnav2909 <nguyenanhvu.2902@gmail.com>
2022-06-09 08:27:49 +0700

    Update page: instructor courses

--- src/pages/instructor-courses/page/instructor-courses.page.tsx
+++ src/pages/instructor-courses/page/instructor-courses.page.tsx
@@ -1,142 +1,168 @@
+import { Pagination, Row } from "antd";
 import { FC, useEffect, useState } from "react";
+import { BiSearch } from "react-icons/bi";
+import { FiClock } from "react-icons/fi";
 import { Link } from "react-router-dom";
-import CourseApi, { CoursesOfInstructor } from "../../../api/course.api";
+import CourseApi, {
+  CoursesOfInstructorResponse,
+} from "../../../api/course.api";
 import Loading from "../../../components/Loading/Loading.component";
 import { ROUTES } from "../../../utils/constants";
 import { linkThumbnail } from "../../../utils/functions";
+import { StyledWrapper } from "../styles/instructor-courses.styles";
 
 const InstructorCoursesPage: FC = () => {
   const [coursesData, setCoursesData] = useState<{
-    courses: CoursesOfInstructor;
+    courses: null | CoursesOfInstructorResponse["coursesData"];
     loaded: boolean;
   }>({
-    courses: [],
+    courses: null,
     loaded: false,
   });
 
   useEffect(() => {
-    CourseApi.getCoursesByCurrentUser().then((res) => {
-      const { data } = res;
-
+    CourseApi.getCoursesByCurrentUser().then(({ data }) => {
       setCoursesData((state) => ({
         ...state,
         loaded: true,
-        courses: data.coursesData.data,
+        courses: data.coursesData,
       }));
     });
   }, []);
 
+  function onChangePage(page: number) {
+    setCoursesData((state) => ({
+      ...state,
+      loaded: false,
+    }));
+
+    CourseApi.getCoursesByCurrentUser(page).then(({ data }) => {
+      setCoursesData((state) => ({
+        ...state,
+        loaded: true,
+        courses: data.coursesData,
+      }));
+    });
+  }
+
   return (
-    <div className="my-course-section">
-      {
-        !coursesData.loaded ? (
-          <Loading />
-        ) : !coursesData.courses.length ? (
-          <div className="empty">
-            <div className="empty__header">
-              <span>Bạn chưa tạo khóa học nào!</span>
+    <StyledWrapper>
+      {!coursesData.loaded ? (
+        <Loading />
+      ) : (coursesData.courses?.total as number) < 1 ? (
+        <div className="empty">
+          <div className="empty__header">
+            <span>Bạn chưa tạo khóa học nào!</span>
 
-              <Link
-                className="add-new-course btn-style-two"
-                to={ROUTES.CREATE_COURSE}
-              >
-                Tạo khóa học mới
-              </Link>
-            </div>
-
-            <div className="img">
-              <img
-                src="https://cdn.dribbble.com/users/2217210/screenshots/12013698/media/c5149aa66b86b923cbf219707265bf16.jpg"
-                alt=""
-              />
-            </div>
+            <Link className="btn-color-default btn" to={ROUTES.CREATE_COURSE}>
+              Tạo khóa học mới
+            </Link>
           </div>
-        ) : (
-          <div className="menu-courses">
-            <h2>Khóa học của bạn</h2>
 
-            <div className="menu-courses-wrapper">
-              <div className="search">
-                <input type="text" placeholder="Tìm khóa học" />
-                <i className="fas fa-search"></i>
+          <div className="img">
+            <img
+              src="https://cdn.dribbble.com/users/2217210/screenshots/12013698/media/c5149aa66b86b923cbf219707265bf16.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      ) : (
+        <div className="menu-courses">
+          <h2>Khóa học của bạn</h2>
+
+          <div className="menu-courses-wrapper">
+            <div className="search">
+              <input type="text" placeholder="Tìm khóa học" />
+              <div className="icon d-flex align-items-center justify-content-center cursor-pointer">
+                <BiSearch />
               </div>
-
-              <select name="" id="">
-                <option value="">Newest</option>
-                <option value="">A-Z</option>
-              </select>
-
-              <Link
-                to={ROUTES.CREATE_COURSE}
-                className="menu-courses-wrapper__create-course btn-style-two"
-              >
-                Tạo khóa học mới
-              </Link>
             </div>
 
-            <ul>
-              {coursesData.courses.map((course) => {
-                return (
-                  <li key={course.id}>
-                    <div className="img-courses">
-                      <img
-                        style={{ objectFit: "cover" }}
-                        alt={course.title}
-                        src={
-                          course.thumbnail
-                            ? linkThumbnail(course.thumbnail)
-                            : "https://s.udemycdn.com/course/200_H/placeholder.jpg"
-                        }
-                      />
-                    </div>
-                    <div
-                      className={
-                        !course.submit_for_review ? "info info-hover" : "info"
-                      }
-                    >
-                      <h5>{course.title}</h5>
-                      <time>{course.updated_at} </time>
-                      <div className="status">
-                        {course.isPublished ? (
-                          <div>
-                            <strong>Public</strong>
-                            <span>Draft</span>
-                          </div>
-                        ) : (
-                          <div>
-                            <strong>Draft</strong>
-                            <span>Public</span>
-                          </div>
-                        )}
-                      </div>
-                      {!course.submit_for_review ? (
-                        <Link
-                          className="view"
-                          to={ROUTES.course_basics(course.id)}
-                        >
-                          Edit / Manage course
-                        </Link>
-                      ) : undefined}
-                    </div>
-                    {course.submit_for_review ? (
-                      <div className="in-review">
-                        <div className="in-review-top">
-                          <i className="far fa-clock"></i>
-                          <span>Đang được xem xét</span>
-                        </div>
-                        <span>Submitted On {course.updated_at}</span>
-                      </div>
-                    ) : undefined}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )
+            <select name="" id="">
+              <option value="">Mới nhất</option>
+              <option value="">A-Z</option>
+            </select>
 
-        // paginate
-      }
-    </div>
+            <Link
+              to={ROUTES.CREATE_COURSE}
+              className="h-100 btn btn-color-default ml-auto"
+            >
+              Tạo khóa học mới
+            </Link>
+          </div>
+
+          <ul>
+            {coursesData.courses?.data.map((course) => {
+              return (
+                <li key={course.id}>
+                  <div className="img-courses">
+                    <img
+                      style={{ objectFit: "cover" }}
+                      alt={course.title}
+                      src={
+                        course.thumbnail
+                          ? linkThumbnail(course.thumbnail)
+                          : "https://s.udemycdn.com/course/200_H/placeholder.jpg"
+                      }
+                    />
+                  </div>
+                  <div
+                    className={
+                      !course.submit_for_review ? "info info-hover" : "info"
+                    }
+                  >
+                    <h5>{course.title}</h5>
+                    <time>{course.updated_at}</time>
+                    <div className="status">
+                      {course.isPublished ? (
+                        <div>
+                          <strong className="mr-1">Công khai</strong>
+                          <span>Bản nháp</span>
+                        </div>
+                      ) : (
+                        <div>
+                          <span className="mr-1">Công khai</span>
+                          <strong>Bản nháp</strong>
+                        </div>
+                      )}
+                    </div>
+                    {!course.submit_for_review && (
+                      <Link
+                        className="view"
+                        to={ROUTES.course_basics(course.id)}
+                      >
+                        Quản lý khóa học
+                      </Link>
+                    )}
+                  </div>
+                  {!!course.submit_for_review && (
+                    <div className="in-review">
+                      <div className="in-review__content">
+                        <div className="in-review-top d-flex align-items-center">
+                          <FiClock />
+                          <span className="ml-1">Đang được xem xét</span>
+                        </div>
+                        <span>Ngày yêu cầu xét duyệt: {course.updated_at}</span>
+                      </div>
+                    </div>
+                  )}
+                </li>
+              );
+            })}
+          </ul>
+
+          <Row justify="end">
+            <Pagination
+              onChange={onChangePage}
+              pageSize={coursesData.courses?.per_page}
+              defaultCurrent={1}
+              current={coursesData.courses?.current_page}
+              total={coursesData.courses?.total}
+            />
+          </Row>
+        </div>
+      )}
+    </StyledWrapper>
   );
 };
 export default InstructorCoursesPage;
--- src/pages/instructor-courses/styles/instructor-courses.styles.tsx
+++ src/pages/instructor-courses/styles/instructor-courses.styles.tsx
@@ -1,286 +1,252 @@
 import styled from "styled-components";
 
 export const StyledWrapper = styled.div`
-  .overview-container {
-    display: flex;
-    min-width: 1px;
-    margin-bottom: 64px;
+  margin: 20px 0 50px;
+
+  h2 {
+    color: black;
+    font-family: serif;
+    font-size: 3rem;
+  }
+
+  .img {
+    position: relative;
+    height: 400px;
+    text-align: center;
+    img {
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  .empty {
+    &__header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 50px;
+      box-shadow: 0 5px 15px rgba(0, 0, 0, 0.151);
+      margin-bottom: 4rem;
+    }
+  }
+  .popular-courses {
     width: 100%;
-    padding: 3rem;
-
-    .overview-content {
-      width: 100%;
-      .overview-title {
-        display: flex;
-        flex-wrap: wrap;
-        margin-bottom: 3rem;
-        h2 {
-          font-family: "Times New Roman", Times, serif;
-          font-size: 3.5rem;
-          font-weight: 800;
-        }
-      }
-      .metrics-content {
-        width: 100%;
-        span {
-          font-size: 1.7rem;
-          color: rgb(19, 107, 107);
-        }
-        .overview-body {
-          border-color: #ddd;
-          margin-bottom: 21px;
-          background-color: #fff;
-          border: 1px solid rgb(209, 198, 198);
-
-          margin-top: 30px;
-          &:hover {
-            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
-          }
-        }
-        .nav-overview-container {
-          width: 100%;
-          position: relative;
-        }
-        .nav-tabs {
-          display: flex;
-          border-bottom: 1px solid #d1c6c6;
-          .overview_admin {
-            display: flex;
-          }
-          .ant-tabs {
-            width: 100%;
-          }
-          li {
-            z-index: 100;
-
-            display: block;
-            position: relative;
-            padding: 0 15px 1rem;
-
-            button {
-              z-index: 0;
-              width: 100%;
-              background-color: #fff;
-              padding: 1.5rem 3rem 1rem;
-              color: #3e4143;
-            }
-          }
-          .text-midnight-lighter {
-            line-height: 1rem;
-            font-size: 13px;
-          }
-
-          .text-midnight {
-            color: #1c1d1f;
-            font-size: 36px;
-            font-weight: 700;
-            margin-top: 1rem;
-            margin-bottom: 10.5px;
-            line-height: 1.1;
-            color: inherit;
-          }
-          .text-midnight-lighter {
-            line-height: 1.5rem;
-          }
-        }
-
-        .tab-content .date {
-          width: 100%;
-          height: 4rem;
-          align-items: center;
-          justify-content: end;
-          margin-right: 2rem;
-          display: flex;
-          font-size: 1.2rem;
-          select {
-            font-size: 1.2rem;
-            padding: 2px;
-            font-weight: 800;
-          }
-        }
-        .instructor-analytics--chart {
-          padding: 20px;
-          width: 100%;
-          min-height: 500px;
-          position: relative;
-        }
-        .instructor-analytics--date-filter {
-          height: 100%;
-          margin-right: 10px;
-          text-align: right;
-        }
-        .instructor-analytics-message {
-          width: 100%;
-
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-        }
-        .containerChart {
-          display: none;
-          height: 600px;
-          canvas {
-            padding: 10rem 5rem;
-          }
-        }
-        .instructor-analytics--chart-footer {
-          text-align: center;
-          width: 100%;
-          border-top: 1px solid #6a6f73;
-          padding: 5px 0;
-          a {
-            padding: 6px 10px;
-            font-size: 15px;
-            line-height: 1.35135;
-
-            i {
-              font-size: 1.1rem;
-              color: black;
-
-              margin-left: 2px;
-            }
-          }
-        }
+    margin: auto;
+  }
+  .popular-courses .courses {
+    display: flex;
+    margin: 0 -10px;
+  }
+  .popular-courses .courses .wrap {
+    border-radius: 15px;
+    position: relative;
+    width: calc(33.333% - 20px);
+    margin: 10px 10px;
+    min-height: 290px;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    cursor: pointer;
+    &:hover {
+      box-shadow: 0 0 10px var(--boxshadow);
+    }
+    h4 {
+      font-size: 16px;
+      margin-bottom: 10px;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      overflow: hidden;
+      a {
+        color: var(--text-color);
       }
     }
   }
-  .body-overview {
-    padding: 0;
-    .footer {
-      .footer-content {
-        max-width: 1200px;
+
+  .container {
+    padding: 10px;
+    flex-grow: 1;
+  }
+
+  .menu-courses {
+    padding: 20px 30px;
+
+    .menu h3 {
+      color: var(--color-muted);
+    }
+    > ul li {
+      margin-top: 20px;
+      height: 120px;
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+      border: 1px solid rgb(206, 206, 206);
+      transition: all 0.1s linear;
+      position: relative;
+    }
+    > ul li .img-courses {
+      height: 100%;
+      flex-shrink: 0;
+      width: 120px;
+
+      background-color: var(--background);
+      overflow: hidden;
+      img {
+        height: 100%;
         width: 100%;
-        margin: auto;
+        object-fit: cover;
+        height: 100%;
       }
     }
 
-    .menu {
-      height: 100vh;
-      z-index: 100;
-      background: var(--color-white);
-      padding-bottom: 5rem;
-      background-color: black !important;
-      width: 60px;
-
-      ul {
-        height: 100vh;
-        z-index: 9999;
-        width: 100%;
+    &-wrapper {
+      height: 48px;
+      margin: 5rem 0;
+      display: flex;
+      align-items: center;
+      .search {
         display: flex;
-        align-items: center;
-        flex-direction: column;
-        backface-visibility: hidden;
-        .hover {
-          transition: width 0.6s, visibility 0.3s;
-          background-color: black;
-          width: 0px;
-          position: absolute;
-          top: 0;
-          left: 0;
-          height: 100%;
-          visibility: hidden;
-          &__item:hover {
-            background-color: rgb(63, 61, 61);
-          }
-          a {
-            position: relative;
-            i {
-              margin-right: 1rem;
-            }
-            i {
-              transform: translateX(-50%);
-              left: 30px;
-              position: absolute;
-            }
-            .logo {
-              transform: translateX(-50%);
-              left: 30px;
-              position: absolute;
-
-              &__container {
-                transform: translateX(-50%);
-                left: 30px;
-                position: absolute;
-                span {
-                  line-height: normal;
-                }
-                .first-letter {
-                  opacity: 0;
-                }
-                .full-text {
-                  transition: opacity 0.4s;
-                  opacity: 0.6;
-                  top: 0;
-                  left: 0;
-                  position: absolute;
-                }
-              }
-            }
-
-            span {
-              margin-left: 2rem;
-              line-height: 1;
-              white-space: nowrap;
-            }
-          }
-        }
-        .logo {
-          span {
-            margin-left: 0 !important;
-          }
-          font-size: 3rem;
-          font-weight: 700;
-        }
-        .menu-hover {
+        height: 100%;
+        margin-right: 3rem;
+        input {
+          outline: none;
+          padding: 0 0.8rem;
           position: relative;
         }
-        &:hover {
-          z-index: 9999;
-          > li {
-            visibility: hidden;
-          }
-          .hover {
-            width: 320px;
-            visibility: visible;
-          }
-          .full-text {
-            opacity: 1 !important;
-          }
-        }
-
-        a {
-          height: 60px;
+        .icon {
+          background: #000;
+          width: 4.8rem;
+          height: 100%;
           color: #fff;
-          align-items: center;
-          display: flex;
         }
+      }
+      input,
+      select {
+        border: 1px solid #000;
+        height: inherit;
+      }
+      select {
+        padding: 0 1rem;
       }
     }
   }
-  .main-overview-page {
-    margin: 0 !important;
-    max-width: 100% !important;
-    .sidebar-overview {
-      z-index: 1 !important;
-      height: 100vh;
-      background-color: #dfdfdf !important;
-      margin-left: 60px !important;
+  .rating-content {
+    flex-shrink: 0;
+    margin-left: 2rem;
+  }
+  .menu-courses .info {
+    padding: 10px;
+    max-width: 600px;
+    width: 100%;
+    margin-left: 15px;
+    align-self: flex-start;
+    height: 100%;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    &.info-hover {
+      max-width: none;
+      &:hover {
+        &::before,
+        .view {
+          opacity: 1;
+          visibility: visible;
+        }
+      }
+      &::before {
+        content: "";
+        left: 0;
+        top: 0;
+        position: absolute;
+        background-color: rgba(255, 255, 255, 0.9);
+        width: 100%;
+        height: 100%;
+        opacity: 0;
+        visibility: hidden;
+      }
+    }
+    h5 {
+      margin-bottom: 5px;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      overflow: hidden;
+    }
+    time {
+      font-size: 13px;
+      color: rgb(54, 54, 54);
+    }
+    .status {
+      font-size: 14px;
+      margin-top: auto;
     }
   }
-  .main-overview-content {
-    box-shadow: none !important;
-    max-width: 1200px;
-    width: 100% !important;
-    margin: 20px auto 50px;
-    min-height: 100vh;
+  .isPublished {
+    margin-top: 1rem;
+    span {
+      font-weight: 700;
+      color: black;
+    }
   }
-  .sidebar-menu {
-    position: fixed;
-    height: 100vh;
-    z-index: 9999;
+  .in-review {
+    // flex-grow: 3;
+    position: absolute;
+    width: 100%;
+    top: 0;
+    left: 0;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    z-index: 10;
+
+    &::before {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      z-index: 12;
+      content: "";
+      left: 0;
+      background-color: rgba(143, 134, 134, 0.349);
+    }
+
+    &__content {
+      width: 100%;
+      max-width: 320px;
+      margin: auto;
+      position: relative;
+      z-index: 20;
+      color: #000;
+    }
+
+    span:last-child {
+      font-size: 1.4rem;
+    }
+    .in-review-top {
+      svg {
+        font-size: 1.4rem;
+        color: rgb(55, 190, 22);
+      }
+      span {
+        color: #000;
+        font-size: 1.6rem;
+        font-weight: 600;
+      }
+    }
   }
-  .activeChart {
-    display: block !important;
+  .view {
+    opacity: 0;
+    visibility: hidden;
+    color: rgb(146, 47, 146);
+
+    display: inline-block;
+
+    font-weight: 700;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
 `;
