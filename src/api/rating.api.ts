b2020f6e2fabd01ce683e5c714eb63616a539db2
devnav2909 <nguyenanhvu.2902@gmail.com>
2022-06-13 11:20:05 +0700

    Add rating api: rate

--- src/api/rating.api.ts
+++ src/api/rating.api.ts
@@ -1,11 +1,21 @@
 import { RatingPagination } from "../ts/types/course.types";
 import axiosClient from "../utils/axios";
 
+export interface Rate {
+  course_id: string | number;
+  content: string;
+  rating: 1 | 2 | 3 | 4 | 5;
+}
+
 class Rating {
   get = async (courseId: number | string, page?: number) => {
     return axiosClient.get<{ rating: RatingPagination }>(
       `/course/${courseId}/rating${page ? "?page=" + page : ""}`
     );
+  };
+
+  rate = async (data: Rate) => {
+    return axiosClient.post("/course/rate", data);
   };
 }
 
