1d4d9a5dbcf4713ca61d1c1a01f2008fbe2b4da8
devnav2909 <nguyenanhvu.2902@gmail.com>
2022-07-10 22:20:09 +0700

    Update layout: instructor view

--- src/layouts/instructor-view.layout.tsx
+++ src/layouts/instructor-view.layout.tsx
@@ -1,19 +1,18 @@
 import {
   BarChartOutlined,
+  CommentOutlined,
+  DollarCircleOutlined,
   FundProjectionScreenOutlined,
-  UsergroupAddOutlined,
-  VideoCameraOutlined,
 } from "@ant-design/icons";
 import { Breadcrumb, Dropdown, Layout, Menu } from "antd";
-import SubMenu from "antd/lib/menu/SubMenu";
 import { FC, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import Notification from "../components/TopNav/Notification.component";
-import { UserImage } from "../components/TopNav/TopNav.component";
-import { useTypedSelector } from "../hooks/redux.hooks";
-import { Role } from "../ts/types/user.types";
-import { ROUTES } from "../utils/constants";
+import Notification from "components/TopNav/Notification.component";
+import { UserImage } from "components/TopNav/TopNav.component";
+import { useTypedSelector } from "hooks/redux.hooks";
+import { Role } from "ts/types/user.types";
+import { ROUTES } from "utils/constants";
 
 const StyledNavTop = styled.nav`
   .nav-content {
@@ -65,6 +64,15 @@ const StyledLogo = styled.div`
   padding: 10px;
 `;
 
+const StyledMain = styled.main`
+  border-radius: 16px;
+  box-shadow: rgb(145 158 171 / 20%) 0px 0px 2px 0px,
+    rgb(145 158 171 / 12%) 0px 12px 24px -4px;
+  padding: 20px;
+  background-color: #fff;
+  min-height: 100vh;
+`;
+
 const OverviewLayout: FC = ({ children }) => {
   const { Content, Sider } = Layout;
 
@@ -78,145 +86,115 @@ const OverviewLayout: FC = ({ children }) => {
   };
 
   return (
-    <>
-      <Layout style={{ minHeight: "100vh" }}>
-        <Sider
-          collapsible
-          collapsed={collapsed}
-          onCollapse={onCollapse}
-          width={280}
-        >
-          <StyledLogo>
-            <Link className="logo" to={ROUTES.home(role)}>
-              <span>Coursewe</span>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={onCollapse}
+        width={280}
+      >
+        <StyledLogo>
+          <Link className="logo" to={ROUTES.home(role)}>
+            <span>Coursewe</span>
+          </Link>
+        </StyledLogo>
+        <Menu theme="dark" mode="inline">
+          <Menu.Item key="1">
+            <Link to={ROUTES.OVERVIEW}>
+              <BarChartOutlined style={{ fontSize: "22px" }} />
+              <span>Tổng quan</span>
             </Link>
-          </StyledLogo>
-          <Menu theme="dark" mode="inline">
-            <Menu.Item key="1">
-              <Link to={ROUTES.OVERVIEW}>
-                <BarChartOutlined style={{ fontSize: "22px" }} />
-                <span>Tổng quan</span>
-              </Link>
-            </Menu.Item>
-            {role === "user" && (
-              <Menu.Item key="2">
-                <Link to={ROUTES.INSTRUCTOR_COURSES}>
-                  <FundProjectionScreenOutlined style={{ fontSize: "22px" }} />
-                  <span>Quản lý khóa học</span>
-                </Link>
-              </Menu.Item>
-            )}
-            {role === "admin" && (
-              <>
-                <Menu.Item key="3">
-                  <Link to={ROUTES.ADMIN_REVIEW}>
-                    <VideoCameraOutlined style={{ fontSize: "22px" }} />
-                    <span>Xét duyệt khóa học</span>
-                  </Link>
-                </Menu.Item>
+          </Menu.Item>
 
-                <SubMenu
-                  key="sub1"
-                  title={
-                    <span>
-                      <UsergroupAddOutlined style={{ fontSize: 20 }} />
-                      <span>Quản lý người dùng</span>
-                    </span>
-                  }
-                >
-                  <Menu.Item key="4">Học viên</Menu.Item>
-                  <Menu.Item key="5">Giảng viên</Menu.Item>
-                  <Menu.Item key="6">
-                    <Link to={ROUTES.QUALITY_REVIEW}>Đội ngũ chuyên môn</Link>
-                  </Menu.Item>
-                </SubMenu>
-              </>
-            )}
+          <Menu.Item key="2">
+            <Link to={ROUTES.INSTRUCTOR_COURSES}>
+              <FundProjectionScreenOutlined style={{ fontSize: "22px" }} />
+              <span>Quản lý khóa học</span>
+            </Link>
+          </Menu.Item>
 
-            {/* <Menu.Item key="3">
-              <Link to={ROUTES.ADMIN_REVIEW}>
-                <VideoCameraOutlined style={{ fontSize: "22px" }} />
-                <span>Quản lý người dùng</span>
-              </Link>
-            </Menu.Item> */}
-          </Menu>
-        </Sider>
-        <Layout>
-          <div>
-            <StyledNavTop className="dashboard-nav">
-              <div className="nav-content">
-                <Breadcrumb style={{ margin: "16px 0" }}>
-                  <Breadcrumb.Item>User</Breadcrumb.Item>
-                  <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                </Breadcrumb>
+          <Menu.Item key="3">
+            <Link to={ROUTES.INSTRUCTOR_REVENUE}>
+              <DollarCircleOutlined style={{ fontSize: "22px" }} />
+              <span>Doanh thu khóa học</span>
+            </Link>
+          </Menu.Item>
 
-                <div className="user">
-                  <div className="notification">
-                    <Notification />
-                  </div>
+          <Menu.Item key="4">
+            <Link to={ROUTES.REVIEW_FILTER}>
+              <CommentOutlined style={{ fontSize: "22px" }} />
+              <span>Học viên đánh giá</span>
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout>
+        <div className="mb-2">
+          <StyledNavTop className="dashboard-nav">
+            <div className="nav-content">
+              <Breadcrumb style={{ margin: "16px 0" }}>
+                <Breadcrumb.Item>User</Breadcrumb.Item>
+                <Breadcrumb.Item>Bill</Breadcrumb.Item>
+              </Breadcrumb>
 
-                  <div className="user-dropdown">
-                    <Dropdown
-                      arrow
-                      placement="bottomRight"
-                      getPopupContainer={(): HTMLElement =>
-                        document.querySelector(".user-dropdown") as HTMLElement
-                      }
-                      trigger={["hover"]}
-                      overlayStyle={{ minWidth: 260 }}
-                      overlay={
-                        <Menu style={{ padding: 10 }}>
-                          <Menu.Item key="0">
-                            <div className="mb-3">
-                              <div className="profile-content">
-                                <div className="profile-info">
-                                  <Link to={ROUTES.PROFILE} className="d-flex">
-                                    <UserImage />
-                                    <div className="account ml-1">
-                                      <b className="d-block">
-                                        {profile?.fullname}
-                                      </b>
-                                      <span className="d-block">
-                                        {profile?.email}
-                                      </span>
-                                    </div>
-                                  </Link>
-                                </div>
+              <div className="user">
+                <div className="notification">
+                  <Notification />
+                </div>
+
+                <div className="user-dropdown">
+                  <Dropdown
+                    arrow
+                    placement="bottomRight"
+                    getPopupContainer={(): HTMLElement =>
+                      document.querySelector(".user-dropdown") as HTMLElement
+                    }
+                    trigger={["hover"]}
+                    overlayStyle={{ minWidth: 260 }}
+                    overlay={
+                      <Menu style={{ padding: 10 }}>
+                        <Menu.Item key="0">
+                          <div className="mb-3">
+                            <div className="profile-content">
+                              <div className="profile-info">
+                                <Link to={ROUTES.PROFILE} className="d-flex">
+                                  <UserImage />
+                                  <div className="account ml-1">
+                                    <b className="d-block">
+                                      {profile?.fullname}
+                                    </b>
+                                    <span className="d-block">
+                                      {profile?.email}
+                                    </span>
+                                  </div>
+                                </Link>
                               </div>
                             </div>
-                          </Menu.Item>
-                          <Menu.Item key="1">
-                            <Link to={ROUTES.PROFILE}>Thông tin cá nhân</Link>
-                          </Menu.Item>
-                          <Menu.Item key="2">
-                            <a href={ROUTES.SIGN_OUT}>Đăng xuất</a>
-                          </Menu.Item>
-                        </Menu>
-                      }
-                    >
-                      <div className="cursor-pointer profile-img">
-                        <UserImage />
-                      </div>
-                    </Dropdown>
-                  </div>
+                          </div>
+                        </Menu.Item>
+                        <Menu.Item key="1">
+                          <Link to={ROUTES.PROFILE}>Thông tin cá nhân</Link>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                          <a href={ROUTES.SIGN_OUT}>Đăng xuất</a>
+                        </Menu.Item>
+                      </Menu>
+                    }
+                  >
+                    <div className="cursor-pointer profile-img">
+                      <UserImage />
+                    </div>
+                  </Dropdown>
                 </div>
               </div>
-            </StyledNavTop>
-          </div>
-          <Content style={{ margin: "0 16px" }}>
-            <main className="pd-2">{children}</main>
-          </Content>
-        </Layout>
+            </div>
+          </StyledNavTop>
+        </div>
+        <Content style={{ margin: "0 16px" }}>
+          <StyledMain>{children}</StyledMain>
+        </Content>
       </Layout>
-      );
-    </>
-    // <StyledWrapper>
-    //   <NavTop />
-    //   <div className="main-page spacing-top-nav">
-    //     <SideBarOverview />
-    //     <main>{children}</main>
-    //   </div>
-    // </StyledWrapper>
+    </Layout>
   );
 };
 export default OverviewLayout;
